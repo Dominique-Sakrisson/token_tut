ab9009d8833312606ef16233d80353567ba5692b
Dominique Sakrisson <domsakrisson@gmailcom>
2021-05-20 20:54:12 -0700

    Basic contract framework

---
+++ truffle.js
@@ -0,0 +1,11 @@
+module.exports ={
+//see http://truffleframework.com/docs/advanced/configuration
+// to customize truffle config
+    network: {
+        development:{
+            host: '1237.0.0.1',
+            port: '7545',
+            newtwork_id: '*' //match any network id
+        }
+    }
+};
